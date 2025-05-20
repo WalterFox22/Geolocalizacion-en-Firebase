c7961308469061f921af2c0374ba4f9ea2fc731c
Walter <walteracero005@gmail.com>
2025-05-20 15:17:26 -0500

    Implemenatcion de seguridad

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-   apiKey: "AIzaSyCXAKSQX9DI__GTmPK4cMTj8jmZ9OoEJJk",
-  authDomain: "amobil-608b6.firebaseapp.com",
-  projectId: "amobil-608b6",
-  storageBucket: "amobil-608b6.firebasestorage.app",
+   apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
   messagingSenderId: "543976213850",
-  appId: "1:543976213850:web:d724c5dfddbb5f31af3763",
-  measurementId: "G-XZ229WJJTX"
+  appId: "",
+  measurementId: ""
   }
 };
 
